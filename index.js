52f1998e3f901b5027ca5a272f8b89a61123e762
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-03 16:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
   return drivers.filter(function (name){
-    return 
+    return name.toLowerCase() = string.toLowerCase()
   })
 }

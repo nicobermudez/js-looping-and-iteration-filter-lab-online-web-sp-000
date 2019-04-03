785a28d2a6d5f0ec2d37f9b7ce3b9d152ecbf92c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-03 16:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function fuzzyMatch(drivers, string) {
 
 function matchName(drivers, string) {
   return drivers.filter(function(driver) {
-    return driver.name.toLowerCase() === name.toLowerCase()
+    return driver.name.toLowerCase() === string.toLowerCase()
   })
 }
